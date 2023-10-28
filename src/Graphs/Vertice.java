1de4d4c84a279d5b936353de2a2228256126221c
AsierEP <asier.errasti@correo.unimet.edu.ve>
2023-10-28 10:10:41 -0400

    añadido las UIs a los grafos

---
+++ src/Graphs/Vertice.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Graphs;
+
+/**
+ *
+ * @author User
+ */
+public class Vertice {
+        private String nombre;
+    private ListaSimple relaciones;
+
+    public Vertice(String nombre) {
+        this.nombre = nombre;
+        this.relaciones = new ListaSimple();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ListaSimple getRelaciones() {
+        return relaciones;
+    }
+
+    public void setRelaciones(ListaSimple relaciones) {
+        this.relaciones = relaciones;
+    }
+    
+    
+    
+    
+    
+
+}
